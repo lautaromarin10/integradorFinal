98f7ff125371053449788c9df7a0e64639fe17d4
Matias Luna <matiluna09@gmail.com>
2025-11-15 13:39:48 -0300

    cambio Base

--- src/Entities/Base.java
+++ src/Entities/Base.java
@@ -4,43 +4,37 @@
  */
 package Entities;
 
-/**
- *
- * @author lautaromarin
- */
-public class Base {
-    private long id;
+import java.io.Serializable;
+
+public class Base implements Serializable {
+
+    private Long id; 
     private boolean eliminado;
-   
-  
-    //constructor completo
-    public Base(long id, boolean eliminado){
+
+    // constructor completo
+    public Base(Long id, boolean eliminado){ 
         this.id = id;
         this.eliminado = eliminado;
     }
-    
-    //constructor por defecto
+
+    // constructor por defecto
     protected Base(){
         this.eliminado = false;
     }
-    
-    
-    public void setId(long id){
+
+    public void setId(Long id){ 
         this.id = id;
     }
-    
-    public long getId(){
+
+    public Long getId(){ 
         return id;
     }
-    
-    
+
     public void setEliminado(boolean eliminado){
         this.eliminado = eliminado;
     }
-    
+
     public boolean estaEliminado(){
         return eliminado;
     }
-    
-    
 }
